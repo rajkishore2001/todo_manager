f5c0875bde52194f812c260957564d9c6af1477f
rajkishore2001 <rajkishore4kv@gmmail.com>
2020-04-01 17:14:36 +0530

    single todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,13 @@
 # todos_controller.rb
 class TodosController < ApplicationController
   def index
-    render plain: Todo.all.to_a
+    render plain: Todo.all.map { |todo| todo.to_pleasant_string }.join("\n")
+  end
+
+  def show
+    id = params[:id]
+    todo = Todo.find(id)
+    render plain: todo.to_pleasant_string
+    #render plain: "local id #{id}"
   end
 end
