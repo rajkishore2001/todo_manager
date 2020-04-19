4266739a2bf014bc8eaf8fe8f1c00ef6230ed0ad
rajkishore2001 <rajkishore4kv@gmmail.com>
2020-04-20 03:09:14 +0530

    Added html,css by partial rendering

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,24 @@ class Todo < ActiveRecord::Base
     is_completed = completed ? "[x]" : "[ ]"
     "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_completed} "
   end
+
+  def due_today?
+    due_date == Date.today
+  end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.overdue
+    all.where("due_date < ?", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
 end
